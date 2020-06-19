049d2fdbe0c5a28b404d9406204425875c476b7e
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-19 12:02:51 -0700

    Updates

---
+++ precache-manifest.5421f5d5a254cc7aae8c448aaa62b240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59171952ee0e4c31226aa037fe87a006",
+    "url": "/petreact/index.html"
+  },
+  {
+    "revision": "3c2b9cbe5a8e22470f15",
+    "url": "/petreact/static/js/2.8301a7f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/petreact/static/js/2.8301a7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18715abbe036f1bd58f9",
+    "url": "/petreact/static/js/main.d766cc58.chunk.js"
+  },
+  {
+    "revision": "59342f882d436d8ae767",
+    "url": "/petreact/static/js/runtime-main.58f357e1.js"
+  }
+]);
